fb1ec1d1f2931c450864e3108b71641ce2343820
RadhikaManjunath <radhikamanjunath34@gmail.com>
2023-05-17 00:38:37 +0530

    first commit

---
+++ src/components/Vedio.js
@@ -0,0 +1,13 @@
+function Vedio()
+{
+    return(
+        <div>
+            <h2>Portfolio</h2>
+            <video >
+                <source src="">
+                </source>
+            </video>
+        </div>
+    )
+}
+export default Vedio;
